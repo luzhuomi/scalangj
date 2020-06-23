3504366febad07de338ab92a2831e5fe9435f865
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2020-06-23 09:35:05 +0800

    updated

--- src/main/scala/com/github/luzhuomi/scalangj/Syntax.scala
+++ src/main/scala/com/github/luzhuomi/scalangj/Syntax.scala
@@ -287,7 +287,7 @@ object Syntax {
       */
     sealed trait ElementValue
     case class EVVal(var_init:VarInit) extends ElementValue
-    case class EVAnn(ann:Annotatoin) extends ElementValue
+    case class EVAnn(ann:Annotation) extends ElementValue
 
 
     // Statements
@@ -309,10 +309,181 @@ object Syntax {
     case class LocalVars(modifiers:List[Modifier], ty:Type, var_decls:List[VarDecl]) extends BlockStmt
 
 
+    /**
+      * A Java statement.
+      */
     sealed trait Stmt
+    /**
+      * A statement can be a nested block.
+      *
+      * @param blk
+      */
     case class StmtBlock(blk:Block) extends Stmt
+    /**
+      * The @if-then@ statement allows conditional execution of a statement.
+      *
+      * @param exp
+      * @param stmt
+      */
     case class IfThen(exp:Exp, stmt:Stmt) extends Stmt
+    /**
+      * The @if-then-else@ statement allows conditional choice of two statements, executing one or the other but not both.
+      *
+      * @param exp
+      * @param then_stmt
+      * @param else_stmt
+      */
     case class IfThenElse(exp:Exp, then_stmt:Stmt, else_stmt:Stmt) extends Stmt
+    /**
+      * The @while@ statement executes an expression and a statement repeatedly until the value of the expression is false.
+      *
+      * @param exp
+      * @param stmt
+      */
     case class While(exp:Exp, stmt:Stmt) extends Stmt
+    /**
+      * The basic @for@ statement executes some initialization code, then executes an expression, a statement, and some
+      * update code repeatedly until the value of the expression is false.
+      * @param init
+      * @param loop_cond
+      * @param post_update
+      * @param stmt
+      */
     case class BasicFor(init:Option[ForInit], loop_cond:Option[Exp], post_update:Option[Exp], stmt:Stmt) extends Stmt
+    /**
+      * The enhanced @for@ statement iterates over an array or a value of a class that implements the @iterator@ interface.
+      *
+      * @param modifiers
+      * @param ty
+      * @param id
+      * @param exp
+      * @param stmt
+      */
+    case class EnhancedFor(modifiers:List[Modifier], ty:Type, id:Ident, exp:Exp, stmt:Stmt) extends Stmt 
+    /**
+      * An empty statement does nothing.
+      */
+    case object Empty extends Stmt
+    /**
+      * Certain kinds of expressions may be used as statements by following them with semicolons:
+      * assignments, pre- or post-inc- or decrementation, method invocation or class instance
+      * creation expressions.
+      *
+      * @param exp
+      */
+    case class ExpStmt(exp:Exp) extends Stmt
+    /**
+      * An assertion is a statement containing a boolean expression, where an error is reported if the expression
+      * evaluates to false.
+      * @param exp
+      * @param msg
+      */
+    case class Assert(exp:Exp, msg:Option[Exp]) extends Stmt
+    /**
+      * The switch statement transfers control to one of several statements depending on the value of an expression.
+      *
+      * @param exp
+      * @param blocks
+      */
+    case class Switch(exp:Exp, blocks:List[SwitchBlock]) extends Stmt
+    /**
+      * The @do@ statement executes a statement and an expression repeatedly until the value of the expression is false.
+      *
+      * @param stmt
+      * @param exp
+      */
+    case class Do(stmt:Stmt, exp:Exp) extends Stmt
+    /**
+      * A @break@ statement transfers control out of an enclosing statement.
+      *
+      * @param id
+      */
+    case class Break(id:Option[Ident]) extends Stmt
+    /**
+      * A @continue@ statement may occur only in a while, do, or for statement. Control passes to the loop-continuation
+      * point of that statement.
+      * @param id
+      */
+    case class Continue(id:Option[Ident]) extends Stmt
+    /**
+      * A @return@ statement returns control to the invoker of a method or constructor.
+      *
+      * @param exp
+      */
+    case class Return(exp:Option[Exp]) extends Stmt
+    /**
+      * A @synchronized@ statement acquires a mutual-exclusion lock on behalf of the executing thread, executes a block,
+      * then releases the lock. While the executing thread owns the lock, no other thread may acquire the lock.
+      * @param exp
+      * @param blk
+      */
+    case class Synchronized(exp:Exp, blk:Block) extends Stmt
+    /**
+      * A @throw@ statement causes an exception to be thrown.
+      *
+      * @param exp
+      */
+    case class Throw(exp:Exp) extends Stmt
+    /**
+      * A try statement executes a block. If a value is thrown and the try statement has one or more catch clauses that
+      * can catch it, then control will be transferred to the first such catch clause. If the try statement has a finally
+      * clause, then another block of code is executed, no matter whether the try block completes normally or abruptly,
+      * and no matter whether a catch clause is first given control.
+      *
+      * @param try_blk
+      * @param catches
+      * @param finally_blk
+      */
+    case class Try(try_blk:Block, catches:List[Catch], finally_blk:Option[Block]) extends Stmt
+    /**
+      * Statements may have label prefixes.
+      *
+      * @param id
+      * @param stmt
+      */
+    case class Labeled(id:Ident, stmt:Stmt) extends Stmt
+
+    /**
+      * If a value is thrown and the try statement has one or more catch clauses that can catch it, then control will be
+      * transferred to the first such catch clause.
+      *
+      * @param params
+      * @param blk
+      */
+    case class Catch(params:FormalParam, blk:Block)
+
+    /**
+      * A block of code labelled with a @case@ or @default@ within a @switch@ statement.
+      *
+      * @param label
+      * @param blk_stmts
+      */
+    case class SwitchBlock(label:SwitchLabel, blk_stmts:List[BlockStmt])
+
+    /**
+      * A label within a @switch@ statement.
+      */
+    sealed trait SwitchLabel
+    case class SwitchCase(exp:Exp) extends SwitchLabel
+    case object Default extends SwitchLabel
+
+    /**
+      * Initialization code for a basic @for@ statement.
+      */
+    sealed trait ForInit
+    case class ForLocalVars(modifiers:List[Modifier], ty:Type, var_decls:List[VarDecl]) extends ForInit
+    case class ForInitExps(exps:List[Exp])
+
+    /**
+      * An exception type has to be a class type or a type variable.
+      */
+    type ExceptionType = RefType // restricted to ClassType or TypeVariable
+
+    /**
+      * Arguments to methods and constructors are expressions.
+      */
+    type Argument = Exp
+
+    
+    sealed trait Exp
 }
