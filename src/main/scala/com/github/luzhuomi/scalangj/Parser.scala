c2bceaa76fc64dd67703692ec837786ba177c384
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2020-07-08 22:09:21 +0800

    working on parser

--- src/main/scala/com/github/luzhuomi/scalangj/Parser.scala
+++ src/main/scala/com/github/luzhuomi/scalangj/Parser.scala
@@ -7,29 +7,50 @@ import com.github.luzhuomi.scalangj._
 import scala.util.parsing.combinator._
 import scala.util.parsing.combinator.syntactical._
 // import lexical.StdLexical
+import scala.util.parsing.input._
+import scala.language.implicitConversions
 
 object Parser extends Parsers {
-    override type Elem = JavaToken 
-    // val lexical: StdLexical = new StdLexical()
+    override type Elem = JavaToken
 
+    def parseCompilationUnit(s:String): ParseResult[CompilationUnit] = {
+        compilationUnit(new Lexer.Scanner(s)) 
+    }
 
-    def lit:Parser[Literal] = accept("literal", {
-        case IntTok(s,i)    => IntLit(i)
-        case LongTok(s,i)   => LongLit(i)
-        case DoubleTok(s,d) => DoubleLit(d)
-        case FloatTok(s,f)  => FloatLit(f)
-        case CharTok(s,c)   => CharLit(c)
-        case StringTok(s, v) => StringLit(s)
-        case BoolTok(s,b)   => BooleanLit(b)
-        case NullTok(s)     => NullLit 
-    })
+    def compilationUnit: Parser[CompilationUnit] = {
+        opt(packageDecl) ~ rep(importDecl) ~ rep(typeDecl) ~ EOF ^^ { 
+            case mpd ~ ids ~ tds ~ _ => CompilationUnit(mpd, ids, tds.flatMap(x => x)) 
+        }
+    }
+
+    def packageDecl:Parser[PackageDecl] = { failure("TODO") }
     
-    def parse(s:String) : Literal = {
+    def importDecl:Parser[ImportDecl] = { failure("TODO")}
+
+    def typeDecl:Parser[Option[TypeDecl]] = { failure("TODO") }
+
+    def literal: Parser[Literal] = 
+    accept(
+        "literal", {
+            case IntTok(s, i)    => IntLit(i)
+            case LongTok(s, i)   => LongLit(i)
+            case DoubleTok(s, d) => DoubleLit(d)
+            case FloatTok(s, f)  => FloatLit(f)
+            case CharTok(s, c)   => CharLit(c)
+            case StringTok(s, v) => StringLit(s)
+            case BoolTok(s, b)   => BooleanLit(b)
+            case NullTok(s)      => NullLit
+        }
+    )
+    /*
+    def parseLiteral(s: String): Literal = {
         lit(new Lexer.Scanner(s)) match {
             case Success(result, _) => result
         }
     }
-    
+    */
+    def parseLiteral(s: String): ParseResult[Literal] = {
+        literal(new Lexer.Scanner(s)) 
+    }
 
 }
-
--- src/test/scala/com/github/luzhuomi/scalangj/TestParser.scala
+++ src/test/scala/com/github/luzhuomi/scalangj/TestParser.scala
@@ -1,8 +1,8 @@
 package com.github.luzhuomi.scalangj
 
 import com.github.luzhuomi.scalangj._
-
-
+import com.github.luzhuomi.scalangj.Parser._ // To unify the base class JavaToken, otherwise === will failed
+import com.github.luzhuomi.scalangj.Syntax._
 import org.scalatest.{FunSuite, Matchers}
 
 
@@ -10,14 +10,12 @@ import org.scalatest.{FunSuite, Matchers}
 // http://matt.might.net/articles/implementation-of-m-expression-parser-in-scala-combinators-without-stdlexical-stdtokenparsers/
 
 
-/*
-class TestLitParser extends FunSuite with Matchers {
+
+class TestLitParser1 extends FunSuite with Matchers {
   val STRING = "1"
-  val TOKEN = IntTok(1)
   val LITERAL = IntLit(1)
-  test("@interface is lexed correlectly") {
-    val result = Parser.lit.apply(List(TOKEN))
+  test("Literal is parse correlectly") {
+    val result = parseLiteral(STRING)
     assert((result.successful) && (result.get === LITERAL))
   }
 }
-*/
