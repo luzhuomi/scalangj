05765fdc4b2d97940ff41334c407282079af6094
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2020-07-14 22:30:12 +0800

    updated test cases

--- src/test/scala/com/github/luzhuomi/scalangj/TestParser.scala
+++ src/test/scala/com/github/luzhuomi/scalangj/TestParser.scala
@@ -70,3 +70,34 @@ class TestParser4 extends FunSuite with Matchers {
     assert((result.successful) && (result.get === NAME))
   }
 }
+
+class TestParser5 extends FunSuite with Matchers {
+  val STRING = """
+  public class Fib
+{
+    public static int fib(int n)
+    {
+	int f1 = 0;
+	int f2 = 1;
+	int i=0;
+	while(i<n) {
+	    int t = f2;
+	    f2 = f1 + f2;
+	    f1 = t;
+	    i++;
+	}
+	return f2;
+    }
+
+    public static void  main(String argv[]) {
+	System.out.println(fib(10));
+    }
+}
+
+  """
+  val CLASSDECL =null
+  test(s"phrase ${STRING} is parsed correctly") {
+    val result = classOrInterfaceDecl.apply(new Lexer.Scanner(STRING))
+    assert((result.successful))
+  }
+}
