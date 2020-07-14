efb608914a714ced380c0626c04c54ddb08ccea7
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2020-07-14 22:07:23 +0800

    fixed test cases for parser

--- src/test/scala/com/github/luzhuomi/scalangj/TestParser.scala
+++ src/test/scala/com/github/luzhuomi/scalangj/TestParser.scala
@@ -41,10 +41,11 @@ public class HelloWorld {
     }
 } 
   """
-  val CLASSDECL = null
+  val CLASSDECL = ClassTypeDecl(ClassDecl_(List(Public),Ident("HelloWorld"),List(),None,List()
+      ,ClassBody(List(MemberDecl_(MethodDecl(List(Public, Static),List(),None,Ident("main"),List(),List(),None,MethodBody(Some(Block(List(BlockStmt_(ExpStmt(MethodInv(MethodCall(Name(List(Ident("System"), Ident("out"), Ident("println"))),List(Lit(StringLit(""""Hello World!"""")))))))))))))))))
   test(s"phrase ${STRING} is parsed correctly") {
     val result = classOrInterfaceDecl.apply(new Lexer.Scanner(STRING))
-    assert((result.successful))// && (result.get === TOKENS))
+    assert((result.successful) && (result.get === CLASSDECL))
   }
 }
 
@@ -53,19 +54,19 @@ class TestParser3 extends FunSuite with Matchers {
   val STRING = """
         System.out.println("Hello World!");
   """
-  val CLASSDECL = null
+  val METHODINV = MethodInv(MethodCall(Name(List(Ident("System"), Ident("out"), Ident("println"))),List(Lit(StringLit(""""Hello World!"""")))))
   test(s"phrase ${STRING} is parsed correctly") {
     val result = methodInvocationExp.apply(new Lexer.Scanner(STRING))
-    assert((result.successful))// && (result.get === TOKENS))
+    assert((result.successful) && (result.get === METHODINV))
   }
 }
 
 
 class TestParser4 extends FunSuite with Matchers {
   val STRING = "System.out.println"
-  val CLASSDECL = null
+  val NAME = Name(List(Ident("System"), Ident("out"), Ident("println")))
   test(s"phrase ${STRING} is parsed correctly") {
     val result = name.apply(new Lexer.Scanner(STRING))
-    assert((result.successful))// && (result.get === TOKENS))
+    assert((result.successful) && (result.get === NAME))
   }
 }
